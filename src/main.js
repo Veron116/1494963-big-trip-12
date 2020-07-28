b3f300e4c8767525f2a8e13766077ce734a27992
Nikushka <veronica.ryzhkova@gmail.com>
2020-07-28 20:01:34 +0700

    добавлены табы в шапку сайта

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const render = (container, template, place) => {
+    container.insertAdjacentHTML(place, template);
+};
+
 const createTripInfoTmplate = () => {
     return (
         `<section class="trip-main__trip-info  trip-info">
@@ -16,11 +20,81 @@ const createTripInfoTmplate = () => {
     );
 };
 
-const render = (container, template, place) => {
-    container.insertAdjacentHTML(place, template);
+const createTabsHeaderTemplate = () => {
+    return (
+        `<nav class="trip-controls__trip-tabs  trip-tabs">
+            <a class="trip-tabs__btn" href="#">Table</a>
+            <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a>
+        </nav>`
+    );
 };
+
+const createTabsFiltersTemplate = () => {
+    return (
+        `<form class="trip-filters" action="#" method="get">
+            <div class="trip-filters__filter">
+                <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+                <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+            </div>
+
+            <div class="trip-filters__filter">
+                <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+                <label class="trip-filters__filter-label" for="filter-future">Future</label>
+                </div>
+
+            <div class="trip-filters__filter">
+                <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+                <label class="trip-filters__filter-label" for="filter-past">Past</label>
+            </div>
+
+            <button class="visually-hidden" type="submit">Accept filter</button>
+      </form>`
+    );
+};
+
 
 const headerElement = document.querySelector('.mjs-header');
 const tripMainElement = headerElement.querySelector('.mjs-trip-main');
+const tabsControlsElement = headerElement.querySelector('.mjs-tabs-controls');
 
 render(tripMainElement, createTripInfoTmplate(), 'afterbegin');
+render(tabsControlsElement, createTabsHeaderTemplate(), 'afterbegin');
+render(tabsControlsElement, createTabsFiltersTemplate(), 'beforeend');
+
+const createSortTemplate = () => {
+    return (
+        `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+            <span class="trip-sort__item  trip-sort__item--day">Day</span>
+
+            <div class="trip-sort__item  trip-sort__item--event">
+                <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
+                <label class="trip-sort__btn" for="sort-event">Event</label>
+            </div>
+
+            <div class="trip-sort__item  trip-sort__item--time">
+                <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+                <label class="trip-sort__btn" for="sort-time">
+                    Time
+                    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+                    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+                    </svg>
+                </label>
+            </div>
+
+            <div class="trip-sort__item  trip-sort__item--price">
+                <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+                <label class="trip-sort__btn" for="sort-price">
+                    Price
+                    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+                    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+                    </svg>
+                </label>
+            </div>
+
+            <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+        </form>`
+    );
+};
+
+const contentElement = document.querySelector('.mjs-content-field');
+render(contentElement, createSortTemplate(), 'afterbegin');
