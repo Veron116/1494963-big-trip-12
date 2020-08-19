af6033beb225e906904124cd1e223a2ec7d6232c
Nikushka <veronica.ryzhkova@gmail.com>
2020-08-19 20:41:53 +0700

    добавлен компонент и его рендер при отсутствующих ивентах

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import TabsHeaderView from './view/tabs-header';
 import TabsFiltersView from './view/tabs-filters';
 import SortView from './view/sort';
 import {TRANSPORT_TYPE, SERVICE_TYPE, CITIES, OFFERS} from './const';
-import {generatePhotoSrcs, render, renderPosition} from './utils';
+import {generatePhotoSrcs, render, RenderPosition} from './utils';
 import {generateEvent} from './mock/event';
 import {filterTabs, filterTypes} from './mock/filter';
-import DayList from './view/day-list';
+import DayListView from './view/day-list';
+import NoEventsView from './view/no-events';
 
 const EVENT_COUNT = 15;
 
@@ -22,16 +23,20 @@ const tripDaysDates = new Set(getDatesStart().map((date) => `${date}`.slice(4, 1
 
 const bodyElement = document.querySelector(`.page-body`);
 const headerElement = new HeaderView();
-render(bodyElement, headerElement.getElement(), renderPosition.AFTERBEGIN);
-render(headerElement.getElement().querySelector(`.trip-controls`), new TabsHeaderView(filterTabs).getElement(), renderPosition.AFTERBEGIN);
-render(headerElement.getElement().querySelector(`.trip-controls`), new TabsFiltersView(filterTypes).getElement(), renderPosition.BEFOREEND);
+render(bodyElement, headerElement.getElement(), RenderPosition.AFTERBEGIN);
+render(headerElement.getElement().querySelector(`.trip-controls`), new TabsHeaderView(filterTabs).getElement(), RenderPosition.AFTERBEGIN);
+render(headerElement.getElement().querySelector(`.trip-controls`), new TabsFiltersView(filterTypes).getElement(), RenderPosition.BEFOREEND);
 
-render(bodyElement, new SortView().getElement(), renderPosition.BEFOREEND);
+render(bodyElement, new SortView().getElement(), RenderPosition.BEFOREEND);
 const tripMainElement = bodyElement.querySelector(`.trip-main`);
-render(tripMainElement, new TripInfoView().getElement(), renderPosition.AFTERBEGIN);
+render(tripMainElement, new TripInfoView().getElement(), RenderPosition.AFTERBEGIN);
 const contentElement = bodyElement.querySelector(`.trip-events`);
-render(
-  contentElement,
-  new DayList(tripDaysDates, events, TRANSPORT_TYPE, SERVICE_TYPE, CITIES, OFFERS, generatePhotoSrcs()).getElement(),
-  renderPosition.BEFOREEND
-);
+if (events.length > 0) {
+  render(
+      contentElement,
+      new DayListView(tripDaysDates, events, TRANSPORT_TYPE, SERVICE_TYPE, CITIES, OFFERS, generatePhotoSrcs()).getElement(),
+      RenderPosition.BEFOREEND
+  );
+} else {
+  render(contentElement, new NoEventsView().getElement(), RenderPosition.BEFOREEND);
+}
