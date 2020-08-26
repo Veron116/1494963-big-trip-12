9ee8930878150e104da754b8213fa012e45f5b75
Nikushka <veronica.ryzhkova@gmail.com>
2020-08-26 20:03:24 +0700

    доработка event презентера

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,10 +1,10 @@
-import Sort from '../view/sort';
-import DayList from '../view/day-list';
-import NoEvents from '../view/no-events';
+import Sort from "../view/sort";
+import DayList from "../view/day-list";
+import NoEvents from "../view/no-events";
 
-import TripDayItem from '../view/trip-day-item';
-import DayEvent from '../view/day-event';
-import EventEdit from '../view/event-edit';
+import TripDayItem from "../view/trip-day-item";
+import DayEvent from "../view/day-event";
+import EventEdit from "../view/event-edit";
 
 import {
   render,
@@ -14,16 +14,15 @@ import {
 import {
   eventsArray,
   renderEventModel
-} from '../utils/event-utils';
+} from "../utils/event-utils";
 import {
   TRANSPORT_TYPE,
   SERVICE_TYPE,
   CITIES
-} from '../const';
-// картинки почему-то беспокнечно грузятся, поэтому пока скрыла
-// import {
-//   generatePhotoSrcs
-// } from '../utils/event-utils';
+} from "../const";
+import {
+  generatePhotoSrcs
+} from "../utils/event-utils";
 
 export default class EventsPresenter {
   constructor(bodyContainer) {
@@ -31,15 +30,12 @@ export default class EventsPresenter {
     this._SortComponent = new Sort();
     this._DayListComponent = new DayList();
     this._NoEventsComponent = new NoEvents();
-    // this._EventEdit = new EventEdit(this._dayEvents, TRANSPORT_TYPE, SERVICE_TYPE, CITIES, OFFERS, generatePhotoSrcs());
     this._eventListeners = this._eventListeners.bind(this);
   }
 
   init() {
     this._renderSort();
     this._renderEvents();
-    this._renderDailyEvents();
-    this._renderEvent();
   }
 
   _renderSort() {
@@ -48,7 +44,7 @@ export default class EventsPresenter {
 
   _renderEvents() {
     const contentElement = this._DayListComponent.getElement();
-    render(this._bodyContainer, contentElement, RenderPosition.BEFOREEND);
+    render(this._SortComponent.getSortContainer(), contentElement, RenderPosition.BEFOREEND);
 
     if (eventsArray.length > 0) {
       this._getTripDayItems().forEach((item) => contentElement.appendChild(item.getElement()));
@@ -58,85 +54,38 @@ export default class EventsPresenter {
   }
 
   _getTripDayItems() {
-
     return renderEventModel().map((model) => {
-      return new TripDayItem(
-          model.date,
-          model.index
-      );
+      const tripDayItem = new TripDayItem(model.date, model.index);
+      const dayEvents = this._createDayEvents(model.dayEvents);
+      const container = tripDayItem.getDayEventContainer();
+      dayEvents.forEach((event) => container.appendChild(event.getElement()));
+      return tripDayItem;
     });
   }
 
-  _createDayEvents() {
-
-    return renderEventModel().map((model) => {
-      return model.dayEvents.map((dayEvent) => {
-        return new DayEvent(dayEvent);
-      });
-    });
-  }
-
-  _renderDailyEvents() {
-    this._getTripDayItems().forEach((dayItem) => {
-
-      this._createDayEvents().forEach((dayEvents) => {
-        dayEvents.forEach((dayEvent) => {
-          dayItem.getDayEventContainer().appendChild(dayEvent.getElement());
-        });
-      });
-    });
-
-  }
-
-  _createEventEdit() {
-    return renderEventModel().map((model) => {
-      return model.dayEvents.map((dayEvent) => {
-        // картинки почему-то беспокнечно грузятся, поэтому пока удалила последний параметр в виде generatePhotoSrcs()
-        return new EventEdit(dayEvent, TRANSPORT_TYPE, SERVICE_TYPE, CITIES);
-      });
-    });
-  }
-
-  _renderEvent() {
-
-    const dayEvents = this._createDayEvents();
-    const dayEventsEdit = this._createEventEdit();
-
-    renderEventModel().forEach((item, index) => {
-
-      dayEvents[index].forEach((dayEvent, dayEventIndex) => {
-        this._eventListeners(dayEvent, dayEventsEdit[dayEventIndex]);
-      });
+  _createDayEvents(dayEvents) {
+    return dayEvents.map((event) => {
+      const dayEvent = new DayEvent(event);
+      const dayEditEvent = new EventEdit(event, TRANSPORT_TYPE, SERVICE_TYPE, CITIES, generatePhotoSrcs());
+      this._eventListeners(dayEvent, dayEditEvent);
+      return dayEvent;
     });
   }
 
   _eventListeners(card, form) {
-    this._createDayEvents().forEach((dayEvents) => {
-      dayEvents.forEach((dayEvent) => {
-        dayEvent.setClickHandler(() => {
-          replace(form, card);
-          document.addEventListener(`keydown`, onEscKeyDown);
-        });
-      });
+    card.setClickHandler(() => {
+      replace(form, card);
+      document.addEventListener(`keydown`, onEscKeyDown);
     });
 
-
-    this._createEventEdit().forEach((dayEvents) => {
-      dayEvents.forEach((dayEvent) => {
-        dayEvent.setEditSubmitHandler(() => {
-          replace(card, form);
-          document.removeEventListener(`keydown`, onEscKeyDown);
-        });
-      });
+    form.setEditSubmitHandler(() => {
+      replace(card, form);
+      document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
-    this._createEventEdit().forEach((dayEvents) => {
-      dayEvents.forEach((dayEvent) => {
-        dayEvent.setResetClickHandler(() => {
-          replace(card, form);
-          document.removeEventListener(`keydown`, onEscKeyDown);
-        });
-      });
+    form.setResetClickHandler(() => {
+      replace(card, form);
+      document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
     const onEscKeyDown = (e) => {
