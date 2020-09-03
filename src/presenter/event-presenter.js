acff5ce4fbcf55581a41871837c239cdfed996ce
Nikushka <veronica.ryzhkova@gmail.com>
2020-09-03 20:46:43 +0700

    добавлены условия проверки для рендера карточек ивентов в event presenter

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -20,13 +20,41 @@ export default class EventPresenter {
   constructor(event, eventContainer) {
     this._event = event;
     this._eventContainer = eventContainer;
-    this._eventComponent = new DayEvent(event);
-    this._eventEditComponent = new EventEdit(event, TRANSPORT_TYPE, SERVICE_TYPE, CITIES, generatePhotoSrcs());
+    this._eventComponent = null;
+    this._eventEditComponent = null;
   }
 
   init() {
-    this._eventContainer.appendChild(this._eventComponent.getElement());
-    this._eventListeners(this._eventComponent, this._eventEditComponent);
+
+    const prevEventComponent = this._eventComponent;
+    const prevEventEditComponent = this._eventEditComponent;
+
+    this._eventComponent = new DayEvent(this._event);
+    this._eventEditComponent = new EventEdit(this._event, TRANSPORT_TYPE, SERVICE_TYPE, CITIES, generatePhotoSrcs());
+
+    if (prevEventComponent === null || prevEventEditComponent === null) {
+      render(this._eventContainer, this._eventComponent, RenderPosition.AFTERBEGIN);
+      this._eventListeners(this._eventComponent, this._eventEditComponent);
+      return;
+    }
+
+    if (this._eventContainer.contains(prevEventComponent.getElement())) {
+      replace(this._eventComponent, prevEventComponent);
+      this._eventListeners(this._eventComponent, this._eventEditComponent);
+    }
+
+    if (this._eventContainer.contains(prevEventEditComponent.getElement())) {
+      replace(this._eventEditComponent, prevEventEditComponent);
+      this._eventListeners(this._eventComponent, this._eventEditComponent);
+    }
+
+    remove(prevEventComponent);
+    remove(prevEventEditComponent);
+  }
+
+  destroy() {
+    remove(this._taskComponent);
+    remove(this._taskEditComponent);
   }
 
   _eventListeners(card, form) {
